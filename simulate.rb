69144a3a305d18d8a51146b8cc7bf9d9411ec834
Sami Samhuri <sami.samhuri@gmail.com>
2011-12-23 18:55:49 -0500

    remove fixed set

--- simulate.rb
+++ simulate.rb
@@ -16,10 +16,6 @@ def main
     opt :verbose, "Print stats after each spin", :short => 'v'
   end
 
-  # options[:fixed_input] = true
-  # options[:input] = [13, 3, 22, 10, 33, 22, 3, 37, 24, 5, 33, 22, 6, 25, 2, 34, 32, 1, 23, 28, 17, 19, 34, 33, 27, 7, 8, 0, 24, 8, 10, 6, 0, 23, 2, 10, 20, 30, 2, 21, 15, 3, 30, 19, 36, 6, 1, 24, 8, 2, 30, 36, 28, 26, 10, 36, 13, 0, 23, 24, 23, 25, 8, 3, 20, 11, 34, 30, 11, 35, 33, 32, 21, 23, 17, 9, 12, 18, 25, 17, 30, 31, 30, 27, 12, 15, 10, 17, 36, 29, 32, 15, 11, 25, 10, 23, 13, 22, 8, 7, 32, 4, 26, 14, 26, 0, 10, 18, 6, 26, 18, 23, 4, 2, 26, 27, 20, 29, 21, 37]
-  # options[:iterations] = options[:input].length
-
   article = options[:style] == 'American' ? 'an' : 'a'
   puts ">>> Simulating #{article} #{options[:style]} style game with #{options[:iterations]} iterations, sleeping after #{options[:misses]} miss#{options[:misses] > 0 ? 'es' : ''}..."
 
